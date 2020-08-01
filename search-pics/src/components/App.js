43b59cf80838ebaa745efb73c07ccfefcfa23ebb
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-01 22:35:36 +0530

    unsplash api

--- search-pics/src/components/App.js
+++ search-pics/src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component  {
+
+
 onSearchSubmit(term) {
-console.log(term);
+axios.get('https://api.unsplash.com/search/photos',{
+params: {query: term},
+headers: {
+Authorization: 'Client-ID UwwCUxZVHUuaMF5qlqpREBRyWTYCuP-HUNxKmSt0RNU'
 }
+
+});
+}
+
+
+
 render (){
 return (
 <div className="ui container" style={{ marginTop: '10px'}}>
